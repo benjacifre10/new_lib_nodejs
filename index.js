a3af2accd7f3eef87ac45a065c2ed18e95877d46
benjacifre10 <43146066+benjacifre10@users.noreply.github.com>
2021-05-03 10:32:12 -0300

    add nodemailer

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-const multi = (number1, number2) => {
-    return number1 * number2;
+'use strict'
+const nodemailer = require('nodemailer');
+
+const send_email = (options) => {
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+    
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: options.aws.host,
+            port: options.aws.port,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: options.aws.user, // generated ethereal user
+                pass: options.aws.pass, // generated ethereal password
+            },
+        });
+    
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: options.email.from, // sender address
+            to: options.email.to, // list of receivers
+            subject: options.email.subject, // Subject line
+            text: options.email.text, // plain text body
+            html: options.email.html, // html body
+        });
+    
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    }
+    
+    main().catch(console.error);
+
+    return "Mail enviado";
 };
 
-exports.multi = multi;
+exports.send_email = send_email;
