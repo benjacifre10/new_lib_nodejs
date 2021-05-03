355ef7e4fb7417d41daa66da873b5e23a1cfa824
benjacifre10 <43146066+benjacifre10@users.noreply.github.com>
2021-05-03 18:27:34 -0300

    simplify code

--- index.js
+++ index.js
@@ -4,22 +4,17 @@ const nodemailer = require('nodemailer');
 const send_email = (options) => {
 
     // async..await is not allowed in global scope, must use a wrapper
-    async function main() {
-        // Generate test SMTP service account from ethereal.email
-        // Only needed if you don't have a real mail account for testing
-        let testAccount = await nodemailer.createTestAccount();
-    
+    async const main = () => {
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
-            host: options.aws.host,
-            port: options.aws.port,
+            host: options.aws.host, //SMTP host
+            port: options.aws.port, // SMTP port(25, 465, 587)
             secure: false, // true for 465, false for other ports
             auth: {
-                user: options.aws.user, // generated ethereal user
-                pass: options.aws.pass, // generated ethereal password
+                user: options.aws.user, // SMTP user
+                pass: options.aws.pass, // SMTP password
             },
         });
-    
         // send mail with defined transport object
         let info = await transporter.sendMail({
             from: options.email.from, // sender address
@@ -28,18 +23,12 @@ const send_email = (options) => {
             text: options.email.text, // plain text body
             html: options.email.html, // html body
         });
-    
-        console.log("Message sent: %s", info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    
-        // Preview only available when sending through an Ethereal account
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     }
     
-    main().catch(console.error);
+    main()
+        .catch((err) => console.error(err))
+        .finally(() => { return err ? err : "Mail enviado" });
 
-    return "Mail enviado";
 };
 
 exports.send_email = send_email;
