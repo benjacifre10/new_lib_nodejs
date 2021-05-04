d8bd11c3a075e780851f106d79eed036e809e593
benjacifre10 <43146066+benjacifre10@users.noreply.github.com>
2021-05-04 08:36:27 -0300

    last update

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 'use strict'
 const nodemailer = require('nodemailer');
 
-const send_email = (options) => {
-
-    // async..await is not allowed in global scope, must use a wrapper
-    const main = async () => {
+const send_email = async (options) => {
+    try {
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
             host: options.aws.host, //SMTP host
             port: options.aws.port, // SMTP port(25, 465, 587)
-            secure: false, // true for 465, false for other ports
+            secure: true, // true for 465, false for other ports
             auth: {
                 user: options.aws.user, // SMTP user
                 pass: options.aws.pass, // SMTP password
             },
         });
         // send mail with defined transport object
-        let info = await transporter.sendMail({
+        return await transporter.sendMail({
             from: options.email.from, // sender address
             to: options.email.to, // list of receivers
             subject: options.email.subject, // Subject line
             text: options.email.text, // plain text body
             html: options.email.html, // html body
         });
+    } catch (error) {
+        return error;
     }
-    
-    main()
-        .catch((err) => console.error(err))
-        .finally(() => { return err ? err : "Mail enviado" });
-
 };
 
 exports.send_email = send_email;
